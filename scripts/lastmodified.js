d1eb6bef6e35bb381db7a94137b91d8d6bf92a33
matthewacline <mattthelinecline@gmail.com>
2023-04-23 02:12:38 -0400

    tried to figure this stupid thing out

---
+++ scripts/lastmodified.js
@@ -0,0 +1,5 @@
+let todaysDate = new Date(document.lastModified);
+let Year = Date.Year();
+
+document.getElementById('copyright').innerHTML = Year
+document.getElementById('lastmodified').innerHTML = todaysDate
